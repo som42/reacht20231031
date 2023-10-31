caa5f46d42f3223e2684a5df663f44f30c5ba2ea
dasom <zzzoo42@naver.com>
2023-10-31 14:32:14 +0900

    종료태그 작성연습 완료, 컴포넌트 여러번 사용 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
-
 function App() {
   return (
     <div>
+      <MyCom1 />
+      <MyCom1 />
+      <MyCom1 />
+    </div>
+  );
+}
 
+function MyCom1() {
+  return (
+    <div style={{ width: "100%" }}>
+      <img
+        style={{ width: "100%" }}
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOOipw82vWgKVehpbbm4is6l6FbwbAMvaZuQ&usqp=CAU
+        "
+        alt="경복궁"
+      />
     </div>
   );
 }
