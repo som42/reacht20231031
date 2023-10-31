ddcd9d38255ab00dfef9f50ac73653ad780abd10
dasom <zzzoo42@naver.com>
2023-10-31 15:10:49 +0900

    jsx에서 js 코드 작성은 {} 사용 연습완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+function MyComp() {
+  const address = "seoul";
+  const country = "korea";
+  const phone = "01082672222";
+  const age = 44;
+  const randomNumber = Math.random();
 
+  // 변수만 쓸수 있는게 아니라 간단한 연산자도 쓸수 있다.
+  return (
+    <>
+      <h1>age는 {age}</h1>
+      <h1>address는 {address}</h1>
+      <h1>country는 {country}</h1>
+      <h1>전화 번호는 {phone}</h1>
+      <h1>난수 : {randomNumber}</h1>
+      <h1>1~10 : {Math.ceil(randomNumber * 10)}</h1>
+    </>
+  );
+}
 
 function App() {
   return (
     <div>
-
+      <MyComp />
     </div>
   );
 }
