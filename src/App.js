f9458327ee5fd21b58da0875580c6fca89d6853f
dasom <zzzoo42@naver.com>
2023-10-31 15:21:11 +0900

    jsx의 {}에서 간단한 연산자 활용

--- src/App.js
+++ src/App.js
@@ -1,57 +1,25 @@
-function MyComp2() {
-  let name = "손흥민";
-  let age = 33;
-
-  // {} jsx에서 js코드 작성하기 위한 기호
-  return (
-    <>
-      <h1>hello react</h1>
-      <h1>hello {name}</h1>
-      <h1>{age} years</h1>
-      <h1>{age * 2} double years</h1>
-      <h1>{name + "선수"}</h1>
-      {/* 그래서 주석이 이렇게 생김*/}
-    </>
-  );
-}
-
 function App() {
-  // jsx에서 태그 사용시 꼭 종료태그를 작성해야 함.
-  // 빈 요소이면 시작 태그에서 종료
-  return (
-    <div>
-      <h1>hello react</h1>
-      <h2>HI REACT</h2>
-      <p>
-        Lorem ipsum dolor.
-        <br /> Lorem ipsum dolor
-      </p>
-      <MyComp />
-      <MyComp2 />
-    </div>
-  );
-}
-// 복잡한 component 따로 해야 간결하게 보인다.
-// 브라우저가 기본으로 제공 해주는 태그(built-in) 소문자로 시작
-// 우리가 만드는 컴포넌트(함수) 는 꼭 대 문자로 시작 해야 한다.
-// return 문을 가져야 한다.
-// return 값은 보통 JSX 코드 ( 꼭 그런건 아니다 )
-function MyComp() {
-  // return 에 jsx 코드 작성 가능
-  // 꼭 하나의 root tag가 존재해야 함.
-  // return 값이 여러 줄이면 ()로 감싸기 ( 별 이유 없으면 항상 감싸기)
-  // root tag가 필요없다면 fragment 사용 가능
+  const number = Math.ceil(Math.random() * 100);
+  const isLarge = number > 50;
+  const isSmall = number <= 50;
 
-  //jsx에서 주석 사용법
-  //{/*  */}
   return (
     <>
-      {/*주석은 이렇게 다세요.*/}
-      {/* frament는 <></> */}
-      <h1>HI COMPONENT</h1>
-      <h2>hello</h2>
+      <h1>난수 : {number}</h1>
+      {/*넘버거 50 크면 큰수, 작으면 작은수/*/}
+      <h1>{number > 50 ? "큰수" : "작은 수"}</h1>
+      <div>{number > 50 ? <BigImage /> : <SmallImage />}</div>
+      <div>{isLarge && <h1>큰 수다 </h1>}</div>
+      <div>{isLarge || <h1>작은 수다!!</h1>}</div>
     </>
   );
+}
+
+function BigImage() {
+  return <h1>큰 이미지</h1>;
+}
+function SmallImage() {
+  return <h3>작은 이미지</h3>;
 }
 
 export default App;
