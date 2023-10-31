944c73d01e6176b4ba709b09fdb5765f61bf483e
dasom <zzzoo42@naver.com>
2023-10-31 15:54:36 +0900

    프로퍼티의 값은 자바스크립트 코드가 될 수 있음.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 function App() {
-  const name : "아름";
+  const num1 = Math.ceil(Math.random() * 6);
+  const num2 = Math.ceil(Math.random() * 6);
+
+  let messageClassName = "lose";
+  let message = "다시 던져 보세요";
+
+  if (num1 === num2) {
+    messageClassName = "WIN";
+    message = "당첨";
+  }
   return (
     <>
-      <h1>이름은 {name}</h1>
-      <h1>이름은 {"아름"}</h1>
+      <h1>1번 주사위 {num1}</h1>
+      <h1>2번 주사위 {num2}</h1>
 
-      <div className={"error"}> Lorem ipsum. </div>
-      <div className="error"> Lorem ipsum. </div>
+      <h1 className={messageClassName}>{message}</h1>
     </>
   );
 }
