6b40e5a242d0a9b8be6f663cf8714224575c8f4c
dasom <zzzoo42@naver.com>
2023-10-31 14:08:45 +0900

    jsx에서 종료 태그 꼭 작성

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-
-
 function App() {
-    return (
-        <div>
-            <h1>hello react</h1>
-            <h2>HI REACT</h2>
-            <MyComp/>
-        </div>
-    );
+  // jsx에서 태그 사용시 꼭 종료태그를 작성해야 함.
+  // 빈 요소이면 시작 태그에서 종료
+  return (
+    <div>
+      <h1>hello react</h1>
+      <h2>HI REACT</h2>
+      <p>
+        Lorem ipsum dolor.
+        <br /> Lorem ipsum dolor
+      </p>
+      <MyComp />
+    </div>
+  );
 }
-    // 복잡한 component 따로 해야 간결하게 보인다.
-    // 브라우저가 기본으로 제공 해주는 태그(built-in) 소문자로 시작
-    // 우리가 만드는 컴포넌트(함수) 는 꼭 대 문자로 시작 해야 한다.
-    // return 문을 가져야 한다.
-    // return 값은 보통 JSX 코드 ( 꼭 그런건 아니다 )
+// 복잡한 component 따로 해야 간결하게 보인다.
+// 브라우저가 기본으로 제공 해주는 태그(built-in) 소문자로 시작
+// 우리가 만드는 컴포넌트(함수) 는 꼭 대 문자로 시작 해야 한다.
+// return 문을 가져야 한다.
+// return 값은 보통 JSX 코드 ( 꼭 그런건 아니다 )
 function MyComp() {
-    // return 에 jsx 코드 작성 가능
-    // 꼭 하나의 root tag가 존재해야 함.
-    // return 값이 여러 줄이면 ()로 감싸기 ( 별 이유 없으면 항상 감싸기)
-    // root tag가 필요없다면 fragment 사용 가능
+  // return 에 jsx 코드 작성 가능
+  // 꼭 하나의 root tag가 존재해야 함.
+  // return 값이 여러 줄이면 ()로 감싸기 ( 별 이유 없으면 항상 감싸기)
+  // root tag가 필요없다면 fragment 사용 가능
 
-    //jsx주석 사용법
-    //{/*  */}
-    return (
-        <>
-            {/* frament는 <></> */}
-            <h1>HI COMPONENT</h1>
-            <h2>hello</h2>
-        </>
-    )
+  //jsx에서 주석 사용법
+  //{/*  */}
+  return (
+    <>
+      {/*주석은 이렇게 다세요.*/}
+      {/* frament는 <></> */}
+      <h1>HI COMPONENT</h1>
+      <h2>hello</h2>
+    </>
+  );
 }
 
 export default App;
