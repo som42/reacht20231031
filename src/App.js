5609991f9c3fee779ebca4598bd3d7a5c07673d7
dasom <zzzoo42@naver.com>
2023-10-31 12:15:45 +0900

    component, 연습 작성 완료

--- src/App.js
+++ src/App.js
@@ -3,9 +3,44 @@
 function App() {
   return (
     <div>
-
+      <MyComponent1 />
+      {/*
+      <div>
+       <h1> hello Component</h1>
+       <div/>
+      */}
+      <MyComponent2 />
+    {/*
+      <p> lorem ipsum alkdjf alkdjf </p>
+      <ul>
+        <li>lorem</li>
+        <li>ipsum</li>
+        <li>dolor</li>
+      </ul>
+    */}
     </div>
   );
+}
+
+function MyComponent1 (){
+  return (
+    <div>
+      <h1>hello Component</h1>
+    </div>
+      )
+}
+
+function MyComponent2 () {
+  return (
+      <>
+        <p>lorem ipsum alkdjf alkdjf</p>
+        <ul>
+          <li>lorem</li>
+          <li>ipsum</li>
+          <li>dalor</li>
+        </ul>
+      </>
+  )
 }
 
 export default App;
