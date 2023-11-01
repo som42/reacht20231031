03369238559d7c52fc95eff9e46a5bbeb4085531
dasom <zzzoo42@naver.com>
2023-11-01 14:27:53 +0900

    export default, import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import MyContainer, { person, team } from "./component/MyContainer";
 
 function App() {
   return (
     <div>
-
+      <MyContainer />
+      <h1>{person.name}</h1>
+      <h1>{person.address}</h1>
+      <h1>{team.title}</h1>
+      <h1>{team.score}</h1>
     </div>
   );
 }
---
+++ src/component/MyContainer.js
@@ -0,0 +1,12 @@
+function MyContainer() {
+  return (
+    <div>
+      <h1>THIS IS CONTAINER</h1>
+    </div>
+  );
+}
+export const person = { name: "여름", address: "독서" };
+
+export const team = { title: "휴우~", score: 2 };
+
+export default MyContainer;
