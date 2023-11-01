2db3fef319cddf7effb4abf49543358017914b66
dasom <zzzoo42@naver.com>
2023-11-01 11:13:35 +0900

    props destructuring assignment 구조 분해 할당 연습시작, 완료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
-
 function App() {
   return (
     <div>
-
+      <MyBox p={"1px"} m={"10"} color="white" bg="#ffd6d1" text="첫번째" />
+      <MyBox p={"1px"} m={"10"} color="blue" bg="#ffd6d1" text="두번째" />
     </div>
   );
 }
 
+function MyBox(props) {
+  let { p, m, bg, color, text } = props;
+  return (
+    <div style={{ padding: p, margin: m, color: color, backgroundColor: bg }}>
+      <p>{text}</p>
+    </div>
+  );
+}
 export default App;
