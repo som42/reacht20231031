ebb1dfb5d313ff7d4e4294504c7d15a2b31edd28
dasom <zzzoo42@naver.com>
2023-11-01 11:34:03 +0900

    props 구조분해할당, 기본값 주기, 연습시작, 완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-
+import { findAllByDisplayValue } from "@testing-library/react";
 
 function App() {
   return (
     <div>
+      <MyComp m="30px" p="10px" color="gold" bg="black" text="hollo reaxt" />
+      <MyComp m="30px" p="10px" text="hollo reaxt" />
+      <MyComp p="30px" bg="black" text="hollo reaxt" />
+      <MyComp p="10px" color="gold" />
+    </div>
+  );
+}
 
+function MyComp({
+  m = "30px",
+  p = "10px",
+  color = "blue",
+  bg = "pink",
+  text = "이거 맞아유?",
+}) {
+  return (
+    <div style={{ padding: p, margin: m, color: color, backgroundColor: bg }}>
+      <p>{text}</p>
     </div>
   );
 }
