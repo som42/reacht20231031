638ee0aabfbbc46728ca4ab0b0d64a23689cf968
dasom <zzzoo42@naver.com>
2023-11-01 10:46:14 +0900

    props destructuring assignment 구조 분해 할당 연습시작, 완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
-
 function App() {
   return (
     <div>
+      <MyElem name="흥민" age={44} team="토트넘" />
+      <MyElem name="강인" age={33} team="파리" />
+    </div>
+  );
+}
 
+function MyElem({ name, age, team }) {
+  return (
+    <div>
+      <h1>
+        {name}에 나이는 {age}이고 {team}에 산다.
+      </h1>
     </div>
   );
 }
