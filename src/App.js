1b136f09c063bfa684ed826fa39e834b2716d1e2
dasom <zzzoo42@naver.com>
2023-11-01 14:35:42 +0900

    여러 파일에서 중복된 nemed export를 import 하는 방법.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-// default export 중가로 없이 하나만 쓰면 된다.
-import MyBox from "./component/MyBox";
-import age, { MyElem, city } from "./component/MyElem";
-import Myvv, { person, country } from "./component/MyBox";
+// 우연히 이름이 같을 수도 있다.
+// 별칭을 줄수 있다.
+import MyBox, { address } from "./component/MyBox";
+import MyElem, { address as myAddress } from "/component/MyElem";
 
+// as 로 named export 값을 import할 때 별칭을 줄 수 있음.
 function App() {
   return (
     <>
-      <h1>{city}</h1>
-      <h1>{age}</h1>
-      <Myvv />
-      <h1>
-        {person.age}, {person.name}
-      </h1>
-      <h1>{country}</h1>
+      <h1>{address}</h1>
+      <h1>{myAddress}</h1>
+      <MyBox />
+      <MyElem />
     </>
   );
 }
