4a1faa9c584df4810edc6819fe23c698ce414605
dasom <zzzoo42@naver.com>
2023-11-01 16:21:50 +0900

    사용할때 클래스명 죽복이 발생할수 있음 -> css.module를 통해 중복방지 해보기

---
+++ src/component/MyBlock.js
@@ -0,0 +1,9 @@
+import style from "../style/yourStryle.module.css";
+
+export function MyBlock() {
+  return (
+    <div>
+      <h1 className={style.error}>Lorem ipsum dolor sit amet.</h1>
+    </div>
+  );
+}
