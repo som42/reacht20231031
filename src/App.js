64bc3db0684d7ac0c05cfdf1735e2c70ac87eb85
dasom <zzzoo42@naver.com>
2023-11-01 10:32:02 +0900

    props destructuring assignment 구조 분해 할당

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 function App() {
-  const myStyle = {};
   return (
     <>
-      <h1
-        className={"note"}
-        style={{ backgroundColor: "blue", color: "white" }}
-        title={"제목 요소 "}
-      >
-        Lorem.
-      </h1>
-      {/*빌트인 컴포넌트*/}
-      <p title={"문단 요소"}>Lorem ipsum dolor.</p>
-      {/*리액트 에서는 밑에 애를 프로퍼티라고 한다.*/}
-      {/*우리가 만든 컴포넌트에 할일은 함수에 적어 주면된다.*/}
-      <MyComp title="내 컴포넌트" name="흥민" />
-      <MyComp title="I am 컴포넌트에요" name="강인" />
+      <MyComp value="흥민" address="신촌" />
+      <MyComp value="여름" address="부산" />
     </>
   );
 }
-// 위에 작성된 프로퍼티는 함수 아귀먼트로 넘어간다.
-// 함수 인자로 사용한 곳에서 넘긴 프로퍼티(property)들이 담김
-//  형태로 담기게 된다.
-// 예) title= "내 컴포넌트" => {title: "내 컴포넌트"}
-// 예) title= "내 컴포넌트" name="흥민" => {title: "내 컴포넌트" , name: "흥민"}
-function MyComp(props) {
-  console.log("MyComp가 받은 property", props);
+//구조 분해 할당으로 할때 자바 스크립트 아귀먼트에 적용 시킨 것
+// 밑에 두개가 같다
+function MyComp(value, address) {
+  // let { value, address } = props;
   return (
     <div>
-      <h1 title={props.title}>hello{props.name}</h1>
+      <h1>
+        {value}는 {address}에 산다
+      </h1>
     </div>
   );
 }
