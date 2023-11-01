b8ef5666bc75f1de57c0d09b5b0167d40be45da8
dasom <zzzoo42@naver.com>
2023-11-01 09:46:29 +0900

    props 작성, 활용 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
-
 function App() {
   return (
     <div>
+      <MyHeader color={"white"} be={"black"} text={"내가 만든 제목"} />
+      <MyHeader color={"orange"} bg={"skyblue"} text={"다시 만든 제목"} />
+    </div>
+  );
+}
 
+function MyHeader(props) {
+  console.log(props);
+  return (
+    <div style={{ color: props.color, backgroundColor: props.bg }}>
+      <h1>{props.text}</h1>
     </div>
   );
 }
