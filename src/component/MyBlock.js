f642dc15f3fc44623a34897268b71cd35b4034a0
dasom <zzzoo42@naver.com>
2023-11-01 16:44:46 +0900

    한 파일 안에 여러 개의 스타일을 만들었을때 적용하는 법

--- src/component/MyBlock.js
+++ src/component/MyBlock.js
@@ -3,7 +3,12 @@ import style from "../style/yourStryle.module.css";
 export function MyBlock() {
   return (
     <div>
-      <h1 className={style.error}>Lorem ipsum dolor sit amet.</h1>
+      <h1 className={`${style.error} ${style.special} ${style.coffee}`}>
+        Lorem ipsum dolor sit amet.
+      </h1>
+      <h1 className={[style.coffee, style.coffee, style.error].join(" ")}>
+        Lorem ipsum dolor sit amet.
+      </h1>
     </div>
   );
 }
