ab4b6e65059522092d79ba55e107f140872668be
dasom <zzzoo42@naver.com>
2023-11-02 16:44:40 +0900

    bootstrap navbar만들어 보기

--- src/App.js
+++ src/App.js
@@ -1,50 +1,61 @@
-import React from "react";
 import {
   Box,
-  Center,
+  Button,
   Flex,
-  FormControl,
-  FormLabel,
-  Image,
-  Switch,
-  Textarea,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
 } from "@chakra-ui/react";
+import React from "react";
+import { ChevronRightIcon, TriangleDownIcon } from "@chakra-ui/icons";
 
 function App(props) {
   return (
-    <div>
-      <Center>
-        <Box w={"480px"}>
-          <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzh_mIq37BRB2UDOgFMnUB1aiddnfeZ2JFVQ&usqp=CAU" />
-        </Box>
-        <Box>
-          <Image
-            boxSize={"100px"}
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzh_mIq37BRB2UDOgFMnUB1aiddnfeZ2JFVQ&usqp=CAU"
-          />
-        </Box>
-        <Box>
-          <Image
-            borderRadius="full"
-            boxSize="200px"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzh_mIq37BRB2UDOgFMnUB1aiddnfeZ2JFVQ&usqp=CAU"
-          />
-        </Box>
-        <Box>
-          <Image
-            src="gibbresh.png"
-            fallbackSrc="https://via.placeholder.com/150"
-          />
-        </Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-      </Center>
-    </div>
+    <Flex
+      px={{ base: "0px", md: "70px", lg: "120px" }}
+      justify="space-between"
+      py="8px"
+      my="10px"
+      bg="red.200"
+    >
+      <Flex gap="7px" align={"center"}>
+        {/*글씨 굵게 as=b*/}
+        <Text fontSize="3xl" as="b">
+          Navbar
+        </Text>
+        <Button variant={"ghost"}>Home</Button>
+        <Button variant={"ghost"}>Link</Button>
+
+        <Menu>
+          <MenuButton
+            variant="ghost"
+            as={Button}
+            rightIcon={<TriangleDownIcon />}
+          >
+            Dropdown
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Download</MenuItem>
+            <MenuItem>Create a Copy</MenuItem>
+            <MenuItem>Mark as Draft</MenuItem>
+            <MenuItem>Delete</MenuItem>
+            <MenuItem>Attend a Workshop</MenuItem>
+          </MenuList>
+        </Menu>
+
+        <Button variant={"ghost"} isDisabled={true}>
+          Disabled
+        </Button>
+      </Flex>
+      <Flex gap="7px" alian={"center"}>
+        <input size="sm" bg="white" />
+        <Button size="sm" variant="outline" colorScheme="blackAlpha">
+          Search
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
 
