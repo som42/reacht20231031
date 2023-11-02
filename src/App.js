7c1ec5dc3d451343169ff7bcbb57eccec345c2ab
dasom <zzzoo42@naver.com>
2023-11-02 11:32:39 +0900

    simplegrid 사용, 반응형 웹 적용

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Flex
-        w={"100%"}
-        h={"50px"}
-        bg={"gray.300"}
-        gap={3}
-        align={"center"}
-        justify={"center"}
-      >
-        <Box bg={"gold"}>Lorem.</Box>
-        <Box bg={"red"}>Itaque!</Box>
-        <Box bg={"blue"}>Rerum!</Box>
-        <Box bg={"green"}>Minus.</Box>
-        <Box bg={"orange"}>Molestiae.</Box>
-      </Flex>
+      <SimpleGrid spacing={"10px"} columns={{ base: 2, md: 3, "2xl": 6 }}>
+        <Box bg="red.100">Lorem.</Box>
+        <Box bg="red.200">Architecto.</Box>
+        <Box bg="red.300">Fugiat.</Box>
+        <Box bg="red.400">Illo!</Box>
+        <Box bg="red.500">Iure.</Box>
+        <Box bg="red.600">Eos!</Box>
+        <Box bg="red.700">Esse?</Box>
+        <Box bg="red.800">Rerum!</Box> <Box bg="red.900">Similique.</Box>
+        <Box bg="red.800">Praesentium.</Box>
+        <Box bg="red.700">Provident.</Box>
+        <Box bg="red.600">Libero?</Box>
+        <Box bg="red.500">Sed?</Box>
+        <Box bg="red.400">Quod!</Box>
+        <Box bg="red.300">Id!</Box>
+        <Box bg="red.200">Velit.</Box>
+        <Box bg="red.100">Ducimus.</Box>
+        <Box bg="red.300">Cum.</Box>
+        <Box bg="red.400">Veniam.</Box>
+        <Box bg="red.500">Cum.</Box>
+        <Box bg="red.600">Sapiente.</Box>
+        <Box bg="red.700">Et?</Box>
+        <Box bg="red.200">Laudantium!</Box>
+        <Box bg="red.300">Ab.</Box>
+        <Box bg="red.400">Minima.</Box>
+        <Box bg="red.700">Veritatis?</Box>
+        <Box bg="red.900">Tenetur.</Box>
+        <Box bg="red.100">Minus?</Box>
+        <Box bg="red.200">Maxime.</Box>
+        <Box bg="red.900">Fugiat?</Box>
+      </SimpleGrid>
     </>
   );
 }
