76d2be20c6de87635d8204624aedfacdb7af5de2
dasom <zzzoo42@naver.com>
2023-11-02 09:34:49 +0900

    필요없는 파일들 삭제함

--- src/component/MyContainer.js
+++
@@ -1,8 +0,0 @@
-import style from "../style/style2.module.css";
-export function MyContainer() {
-  return (
-    <div>
-      <h1 className={style.boxClass}>Lorem ipsum dolor sit amet.</h1>
-    </div>
-  );
-}
