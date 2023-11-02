3fc1e4dcf8cf4c2e1997d4216ed8a43e3c4c7504
dasom <zzzoo42@naver.com>
2023-11-02 14:12:43 +0900

    Modal 사용

--- src/App.js
+++ src/App.js
@@ -1,62 +1,45 @@
 import {
-  Box,
-  SimpleGrid,
-  Table,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
 } from "@chakra-ui/react";
+
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
-      <SimpleGrid columns={1} gap={5}>
-        <Box>
-          <Table variant="striped" colorScheme="red" size={"sm"}>
-            <Thead>
-              <Tr>
-                <Th>Lorem.</Th>
-                <Th>Amet.</Th>
-                <Th>Perferendis!</Th>
-                <Th>Quos!</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Tenetur!</Td>
-                <Td>Non.</Td>
-                <Td>At!</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Laudantium?</Td>
-                <Td>Reprehenderit.</Td>
-                <Td>Assumenda.</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Aut!</Td>
-                <Td>Aut?</Td>
-                <Td>Earum!</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Dicta!</Td>
-                <Td>Velit!</Td>
-                <Td>Ratione.</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Dolore?</Td>
-                <Td>Saepe.</Td>
-                <Td>Maxime?</Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </Box>
-      </SimpleGrid>
+      <Button onClick={onOpen}>매매</Button>
+
+      <Modal
+        closeOnOverlayClick={false}
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        motionPreset="scale"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>과천 자이</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            지금 구매하실려는 아파트가 과천 자이가 맞습니까? 지금 3년 동안
+            비교한 시세보다 30% 떨어진 가격이지만 앞으로 어떡해 될지는 모릅니다.
+            이 아파트 실 거래와, 전세를 비교했을때 매매사기, 전세사기는
+            없었습니다. 안전한 거래로 연결해 드릴가요?
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" onClick={onClose}>
+              ❤️
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
