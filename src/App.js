e2c06a4e314499c8d34975cd3839cb35b83a35ad
dasom <zzzoo42@naver.com>
2023-11-02 15:25:48 +0900

    switch 사용

--- src/App.js
+++ src/App.js
@@ -1,46 +1,26 @@
+import React from "react";
 import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  useDisclosure,
+  Box,
+  Center,
+  Flex,
+  FormControl,
+  FormLabel,
+  Switch,
+  Textarea,
 } from "@chakra-ui/react";
 
-function App() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+function App(props) {
   return (
-    <>
-      <Button onClick={onOpen}>매매</Button>
-
-      <Modal
-        closeOnOverlayClick={false}
-        isOpen={isOpen}
-        onClose={onClose}
-        isCentered
-        motionPreset="scale"
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>과천 자이</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            지금 구매하실려는 아파트가 과천 자이가 맞습니까? 지금 3년 동안
-            비교한 시세보다 30% 떨어진 가격이지만 앞으로 어떡해 될지는 모릅니다.
-            이 아파트 실 거래와, 전세를 비교했을때 매매사기, 전세사기는
-            없었습니다. 안전한 거래로 연결해 드릴가요?
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" onClick={onClose}>
-              ❤️
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
+    <div>
+      <Center>
+        <Box w={"480px"}>
+          <FormControl display={"flex"} alignItems={"center"}>
+            <FormLabel mb={0}>알림 받을 꼬얌?</FormLabel>
+            <Switch size={"lg"} colorScheme="pink" />
+          </FormControl>
+        </Box>
+      </Center>
+    </div>
   );
 }
 
