6d11fbce122f23fc2f0ab77a111d2d71ca7d6b2b
dasom <zzzoo42@naver.com>
2023-11-03 09:35:40 +0900

    children prop Button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Button } from "@chakra-ui/react";
+
+function MyLink({ to, children }) {
+  return (
+    // 버튼 안에 링크 넣어 보기
+    <>
+      <Button as="a" href={to} colorScheme="pink">
+        {children}
+      </Button>
+    </>
+  );
+}
 
 function App() {
   return (
     <>
+      <MyLink to="https://www.naver.com">naver 😌</MyLink>
+      <MyLink to="https://www.daum.net">daum ❤️</MyLink>
     </>
   );
 }
