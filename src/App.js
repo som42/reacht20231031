94fc00c3374b9e64555ab366fa34dd8099b450ab
dasom <zzzoo42@naver.com>
2023-11-03 10:41:26 +0900

    arrow function을prop으로 전달

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
 
-// 프랍으로 받은게 함수면 안에서 사용 할수 있다.
-function MyComp({ to, someProp }) {
-  someProp();
-
-  return (
-    <>
-      <div>
-        <a href={to[0]}>link!!</a>;
-      </div>
-      <div>
-        {/*함수를 넘겨서 이벤트 프로퍼티에 쓸수있다.*/}
-        <Button onClick={someProp}>버튼</Button>
-      </div>
-    </>
-  );
+function MyComp({ children, executeClick }) {
+  return <Button onClick={executeClick}>{children}</Button>;
 }
-
 function App(props) {
+  // 함수를 만들때 =>
   function func1() {
-    console.log("func1 실행됨");
+    console.log("func1 실행");
   }
+  // 함수를 변수에 답는다
+  let func2 = () => {
+    console.log("arrow function 실행 111");
+  };
+  // 변수에 담아서 함수를 넣나 바로 넣어버리나 같다.
   return (
     <div>
-      {/*뭐 든지 들어 갈수 있으니깐 함수도 들어갈수 있다.*/}
-      <MyComp to={["https://www.naver.com"]} someProp={func1}></MyComp>
+      <MyComp executeClick={func1}>Button1</MyComp>
+      <MyComp executeClick={func2}>Button2</MyComp>
+      <MyComp
+        executeClick={() => {
+          console.log("arrow function 실행 222");
+        }}
+      >
+        Button3
+      </MyComp>
     </div>
   );
 }
