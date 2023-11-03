a4eb1099ee29fc1d5a6dc6ec01733b47e2ca27d3
dasom <zzzoo42@naver.com>
2023-11-03 10:21:16 +0900

    함수를 props으로 전달

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-function MyComp({ text, address, children }) {
+import React from "react";
+import { Button } from "@chakra-ui/react";
+
+// 프랍으로 받은게 함수면 안에서 사용 할수 있다.
+function MyComp({ to, someProp }) {
+  someProp();
+
   return (
     <>
-      <p>{text}</p>
-      <p>{address}</p>
-      <p>{children}</p>
+      <div>
+        <a href={to[0]}>link!!</a>;
+      </div>
+      <div>
+        {/*함수를 넘겨서 이벤트 프로퍼티에 쓸수있다.*/}
+        <Button onClick={someProp}>버튼</Button>
+      </div>
     </>
   );
 }
 
 function App(props) {
+  function func1() {
+    console.log("func1 실행됨");
+  }
   return (
     <div>
-      <MyComp text="hello" address="seoul" />
-      some contents, 다른 컴포넌트가 있을 수도 있음.
+      {/*뭐 든지 들어 갈수 있으니깐 함수도 들어갈수 있다.*/}
+      <MyComp to={["https://www.naver.com"]} someProp={func1}></MyComp>
     </div>
   );
 }
