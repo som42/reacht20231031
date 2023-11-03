cc99ecdef6a710c6a6bd3ee278ce66cb48e6253a
dasom <zzzoo42@naver.com>
2023-11-03 16:17:28 +0900

    state 변경해서 input value 할당하기

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
-import { NumberInput, Text } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 
 function App(props) {
   const [number, setNumber] = useState(0);
-
-  const isOdd = number % 2 == 1;
-
-  function handleInputChange(e) {
-    setNumber(Number(e.target.value));
+  function handlePlusButtonClick() {
+    setNumber(number + 1);
   }
-
+  function handleMinusButtonClick() {
+    setNumber(number - 1);
+  }
   return (
     <div>
-      <input type="number" onChange={handleInputChange} />
-      <Text>
-        {isOdd && "홀수"}
-        {isOdd || "짝수"}
-      </Text>
+      {/*더하기 빼기 누를 때마다 값 바뀌게 하기 */}
+      <input type="number" value={number} />
+      <Box>
+        <Button onClick={handlePlusButtonClick}>+</Button>
+        <Button onClick={handleMinusButtonClick}>-</Button>
+      </Box>
+      <Box>
+        <Button onClick={() => setNumber(number + 1)}>+</Button>
+        <Button onClick={() => setNumber(number - 1)}>-</Button>
+      </Box>
     </div>
   );
 }
