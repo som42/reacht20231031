53ac2c1d88470c5afceb0ddb22f26d7386053a72
dasom <zzzoo42@naver.com>
2023-11-03 12:33:44 +0900

    event.preventDefault(): 기본 기능 취소 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-
 function App() {
+  function handleClick(e) {
+    e.preventDefault(); // 월래 해야 하는 일 멈추고
+    // 누르면 네이버로 가게 하기.
+    window.location.href = "https://www.naver.com";
+  }
   return (
     <>
+      <a href="http://www.daum.net" onClick={handleClick}>
+        다음으로 이동
+      </a>
     </>
   );
 }
