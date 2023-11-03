35b2788998bea052210d9ba1ed7a4efbc12430bf
dasom <zzzoo42@naver.com>
2023-11-03 15:18:24 +0900

    예쩨) text input으로 상태 변경

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { useState } from "react";
-import { Button, Text } from "@chakra-ui/react";
+import { Input, Text } from "@chakra-ui/react";
 
 function App(props) {
-  // 현재 바뀔거 초기값
-  let [number, setNumber] = useState(0);
-  function handleClick() {
-    // number++ ;
-    // 상태를 변경할때는 꼭 리액트한테 상태변경해달라는 명령어를 내려야한다.
-    // 상태를 변경할 때 set 메소드를 사용해야 한다.
-    setNumber(number + 1);
+  const [message, setMessage] = useState("");
+
+  function handleInputChange(e) {
+    setMessage(e.target.value);
   }
   return (
     <div>
-      <Button onClick={handleClick}>바꾸기</Button>
-      <Text>{number}</Text>
+      <Input onChange={handleInputChange} />
+      <Text>{message}</Text>
     </div>
   );
 }
