db4352d9860797ede41b38c20b73c0126b92a142
dasom <zzzoo42@naver.com>
2023-11-03 14:57:21 +0900

    state 가 바뀌면 component가 다시 그려짐 (re-render) 상태를 변경할 때 set 메소드를 사용해야함

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
 import React, { useState } from "react";
-import { Button, Input } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
 
 function App(props) {
-  // 상태(state)가 변경(이전 state와 달라짐)되면
-  // Component를 다시 그림 (re render)
-
-  // state : react가 관리(리액트가 제공하는 메소드)하는 component의 값
-  // state 를 얻는 방법 : useState();
-
-  // 배열을 리턴 한다
-  // index 0 : 현재 상태 (state)
-  // index 1 : state 변경할 수 있는 메소드
-  // 상태를 바꿀수 있다.
-  const [message, setMessage] = useState("");
-
-  // 상태가 바뀔 때에만 컴포넌트가 re render됨
-
-  let text = "초기 메세지";
-  function handleInputChange(e) {
-    text = e.target.value;
-    // 이 텍스처 는 변경되지만 div안에 있는 text는 변경되지 않는다
-    // 그럼 왜 ?
-    console.log("text", text);
-  }
-  function handleButtonClick(e) {
-    setMessage("바뀐 상태");
+  let [number, setNumber] = useState(0);
+  function handleClick() {
+    // number++ ;
+    // 상태를 변경할때는 꼭 리액트한테 상태변경해달라는 명령어를 내려야한다.
+    // 상태를 변경할 때 set 메소드를 사용해야 한다.
+    setNumber(number + 1);
   }
   return (
     <div>
-      <Input onChange={handleInputChange} />
-      <p>{text}</p>
-      <Button onClick={handleButtonClick}> 상태 바꾸기</Button>
-      <Button onClick={() => setMessage("")}>빈 문자열로 바꾸기</Button>
-      <p>{message}</p>
+      <Button onClick={handleClick}>바꾸기</Button>
+      <Text>{number}</Text>
     </div>
   );
 }
