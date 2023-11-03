98e56e3cb7c9c274b6c18741f8b45597b03fdcd2
dasom <zzzoo42@naver.com>
2023-11-03 14:11:48 +0900

    event.stopPropagation: event bubbling 막는 기능

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React from "react";
+import { Box, Button, Center } from "@chakra-ui/react";
 
 function App(props) {
   function handleClick(e) {
-    // 브라우저가 해 야하는 기본 기능을 방지, 취소 하고 싶을때
-    e.preventDefault();
-    console.log("다른 일을 시킴");
+    // event bubbling(밖에 있는 이벤트 에게 전달) 막는 메소드
+    e.stopPropagation();
+    console.log(e.target.className);
   }
-  function handleSubmit(e) {
-    // 기본 기능 취소
-    e.preventDefault();
-    console.log("다른 일을 시킴");
-  }
-
   return (
-    <div>
-      <a href="https://www.naver.com" onClick={handleClick}>
-        네이버
-      </a>
-      <div>
-        <form action="https://search.daum.com/search" onSubmit={handleSubmit}>
-          <input type="text" name="q" />
-          <button>검색</button>
-        </form>
-      </div>
-    </div>
+    // 버튼을 클릭하면 Center 클릭한 것과 같은 것일까?
+    // onClick 를 셋다 줬기 때문에 Button 누르면 3번이 실행된다
+    // 이걸 event bubbling(버블링) 이라고 부른다.
+    <Center
+      onClick={handleClick}
+      className="outerBox"
+      w="200px"
+      h="200px"
+      bg="gold"
+    >
+      <Center
+        onClick={handleClick}
+        className="innerBox"
+        w="100px"
+        h="100px"
+        bg="black"
+      >
+        <Button onClick={handleClick} className="button" colorScheme="yellow">
+          Button
+        </Button>
+      </Center>
+    </Center>
   );
 }
 
