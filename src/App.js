d20d8da7ee09db93731f6d486442b7b3c881a64a
dasom <zzzoo42@naver.com>
2023-11-06 10:29:02 +0900

    hook 사용 조건 어겨 보기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  // HOOK : use 로 시작하는 함수
-  // hook 은 컴포넌트 상단에 작성해야함
-  // 조건문(if), 반복문(for), 중첩된 함수안 에 작성하면 안됨
-
-  if (true) {
-  }
-
-  function handle() {
-    // hook 호출 코드 불가
+  const [number, setNumber] = useState(0);
+  if (number > 5) {
+    const [text, setText] = useState("");
   }
   return <div></div>;
 }
