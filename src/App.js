7e74446d4a2280f84a3bf39befd22febb60d6384
dasom <zzzoo42@naver.com>
2023-11-06 12:37:23 +0900

    배열을 상태로 쓸 경우 상태 변경시 새로운 배열로 복사해서 사용해야함

--- src/App.js
+++ src/App.js
@@ -2,30 +2,29 @@ import React, { useState } from "react";
 import { Button, Input, List, ListItem } from "@chakra-ui/react";
 
 function App(props) {
-  const [coffees, setCoffees] = useState([]);
+  const [coffees, setCoffees] = useState(["라떼"]);
   const [text, setText] = useState("");
 
-  function handleBittonClick() {
-    //직전 상태 배열을 조작하면 안되고
+  function handleButtonClick() {
+    // 직전 상태 배열을 조작하면 안되고
     // coffees.push(text);
 
     // 새 배열을 만들어서 set state 해야 함
-    const newCoffees = [...coffees];
-
+    const newCoffees = [...coffees]; // 배열 복사
     newCoffees.push(text);
-    setCoffees(coffees);
+
+    setCoffees(newCoffees);
   }
 
   return (
     <div>
       <Input value={text} onChange={(e) => setText(e.target.value)} />
-      <Button onClick={handleBittonClick}>추가</Button>
+      <Button onClick={handleButtonClick}>추가</Button>
 
       <List>
         {coffees.map((c, index) => (
           <ListItem key={index}>{c}</ListItem>
         ))}
-        ;
       </List>
     </div>
   );
