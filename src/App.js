abe062239c835f720a534df6dce9c7685d382935
dasom <zzzoo42@naver.com>
2023-11-06 12:32:42 +0900

    배열을 상태로 쓸 경우 상태 변경시 새로운 배열로 복사해서 사용해야함

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
-import React from "react";
-import { Box, Link, List, ListItem } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Button, Input, List, ListItem } from "@chakra-ui/react";
 
 function App(props) {
-  //배열을 그리는 방법
-  // 상태 여서 변경 되어야 한다면?
-  const arr = ["pizza", "돈까스", "햄버거", "김치찌개"];
-  const arr2 = ["손흥민", "이강인", "김민재"];
-  const arr3 = ["라떼", "아메리카노", "카푸치노"];
-  const arr4 = [
-    {id: 0, name:"라떼"},
-    {id:1, name: "에스프레소"},
-    ];
+  const [coffees, setCoffees] = useState([]);
+  const [text, setText] = useState("");
 
-  // 자바스크립트에 map 메소드는 새 배열을 리턴해준다.
+  function handleBittonClick() {
+    //직전 상태 배열을 조작하면 안되고
+    // coffees.push(text);
 
-  const listItems = arr.map((item, index) => (
-    <ListItem key={index}>{item}</ListItem>
-  ));
+    // 새 배열을 만들어서 set state 해야 함
+    const newCoffees = [...coffees];
+
+    newCoffees.push(text);
+    setCoffees(coffees);
+  }
 
   return (
     <div>
-      <Box mb={3}>
-        <List>{listItems}</List>
-      </Box>
-      <Box mb={3}>
-        <List>
-          {arr2.map((e, index) => (
-            <ListItem key={index}>{e}</ListItem>
-          ))}
-        </List>
-      </Box>
-      <Box mb={3}>
-        <List>
-          {arr3.map((e, i) => (
-            <ListItem key={i}>{e}</ListItem>
-          ))}
-        </List>
-      </Box>
+      <Input value={text} onChange={(e) => setText(e.target.value)} />
+      <Button onClick={handleBittonClick}>추가</Button>
+
+      <List>
+        {coffees.map((c, index) => (
+          <ListItem key={index}>{c}</ListItem>
+        ))}
+        ;
+      </List>
     </div>
   );
 }
