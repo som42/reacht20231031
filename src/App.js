54499d9fa6493cf8caf361bb32b57ff9a4d2004b
dasom <zzzoo42@naver.com>
2023-11-06 14:17:23 +0900

    배열을 상태로 쓸 경우 상태 변경을 하는 경우 연습 완료,

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import React, { useState } from "react";
+import { Box, Button, List, ListItem } from "@chakra-ui/react";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handButtonClick(item) {
+    console.log(item);
+    // items이 상태를 item 추가하도록 변경
+    // const newitems = [...items];
+    // newitems.push(item);
+
+    // const newitems = [...items, item];
+    // setItems(newitems);
+
+    setItems([...items, item]);
+  }
+
   return (
     <>
+      <Button onClick={() => handButtonClick("커피")}>커피</Button>
+      <Button onClick={() => handButtonClick("케잌")}>케잌</Button>
+      <Button onClick={() => handButtonClick("아이스크림")}>아이스크림</Button>
+      <Button onClick={() => handButtonClick("크로와상")}>크로와상</Button>
+      <Button onClick={() => handButtonClick("단팥빵")}>단팥빵</Button>
+
+      <Box>
+        <List>
+          {items.map((item, index) => (
+            <ListItem key={index}>{item}</ListItem>
+          ))}
+          ;
+        </List>
+      </Box>
     </>
   );
 }
