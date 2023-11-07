c5f218f2c059281ff86818d5c7b81e0d9c01a3ba
dasom <zzzoo42@naver.com>
2023-11-07 10:19:04 +0900

    다른 컴포넌트의 상태 바꾸기 : 공통 부모에게 상태를 두고 사용해야 함 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Box, Button, Text } from "@chakra-ui/react";
 
-function MyComp2(message) {
+function MyComp2({ message }) {
   return (
     <Box>
       <Text>{message}</Text>
@@ -9,22 +9,27 @@ function MyComp2(message) {
   );
 }
 
-function MyComp1() {
+function MyComp1({ onClick }) {
   return (
     <Box>
-      <Button>Text 바꾸기!</Button>
+      <Button onClick={onClick}>텍스트 바꾸기!</Button>
     </Box>
   );
 }
 
 function App(props) {
   const [message, setMessage] = useState("hello");
+
+  function handleButtonClick() {
+    setMessage("안녕하세요");
+  }
+
   return (
     <div>
-      <MyComp1></MyComp1>
-      {/*MyComp1에 있는 버튼이 클릭되면*/}
-      <MyComp2></MyComp2>
-      {/*MyComp2에 있는 text가 바뀌게*/}
+      <MyComp1 onClick={handleButtonClick}></MyComp1>
+      {/* MyComp1 에 있는 버튼이 클릭되면 */}
+      <MyComp2 message={message}></MyComp2>
+      {/*  MyCom2에 있는 text가 바뀌게 */}
     </div>
   );
 }
