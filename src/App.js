d876de38801bd39f5a29dceb207493a55c81f1f8
dasom <zzzoo42@naver.com>
2023-11-08 14:31:11 +0900

    component의 초기렌더링과, 재렌더링

--- src/App.js
+++ src/App.js
@@ -1,15 +1,65 @@
 import React, { useEffect, useState } from "react";
+import { Box, Button, Checkbox, Text } from "@chakra-ui/react";
+import { color } from "framer-motion";
 
-function App(props) {
+function MyComp({ color }) {
   const [number, setNumber] = useState(0);
 
-  // useEffect 사용시 자주 하는 실수
   useEffect(() => {
-    console.log("코드 실행");
-    setNumber(number); // trigger 하는 값을 변경하면 안됨
-  }, [number]);
+    console.log(color + " : initial render");
+  }, []);
 
-  return <div></div>;
+  console.log(color + " : re render");
+
+  return (
+    // eslint-disable-next-line no-undef
+    <Box borderWidth={"10px"} borderColor={color}>
+      <Button onClick={() => setNumber(number + 1)}>증가</Button>
+      <Text>{number}</Text>
+    </Box>
+  );
+}
+
+function App(props) {
+  // 부모가 렌더링 될 때 자식도 렌더링 됨.
+
+  const [number, setNumber] = useState(0);
+  const [secondToggle, setSecondToggle] = useState(true);
+  const [goldToggle, setGoldToggle] = useState(true);
+
+  console.log("부모 re-render");
+
+  return (
+    <div>
+      <Box>
+        <Text>부모</Text>
+        <Button onClick={() => setNumber(number + 1)}>증가</Button>
+        <Text>{number}</Text>
+        <Checkbox
+          defaultChecked={true}
+          onChange={(e) => setSecondToggle(e.target.checked)}
+        ></Checkbox>{" "}
+        검은 박스 토글
+        <Checkbox
+          defaultValue={true}
+          onChange={(e) => setGoldToggle(e.target.checked)}
+        />
+        골드 박스 토글
+      </Box>
+      <Box mt={5}>
+        <Text>자식들</Text>
+        <MyComp color={"pink"} />
+
+        {/*골드 박스는 css 안보이게 해서 다시 보여져서 그대로 있다.*/}
+        {secondToggle && <MyComp color={"black"} />}
+        <Box sx={{ display: goldToggle ? "block" : "none" }}>
+          <MyComp color={"gold"} />
+        </Box>
+
+        {/*상태를 유지하게 하고싶다.*/}
+      </Box>
+    </div>
+  );
 }
 
 export default App;
