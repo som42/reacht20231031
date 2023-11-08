f018b03bb338584ab5de8d37a14b93bdacb35a42
dasom <zzzoo42@naver.com>
2023-11-08 15:15:36 +0900

    중첩된 경로

--- src/App.js
+++ src/App.js
@@ -15,6 +15,18 @@ const routes = createBrowserRouter(
       <Route path="/path1" element={<Box>경로1</Box>} />
       <Route path="/path2" element={<Box>경로2</Box>} />
       <Route path="/path3" element={<Box>경로3</Box>} />
+      {/*공통된 상위 공로가 있다면 하나로 합칠수 있다.*/}
+      <Route path="/main1/path1" element={<Box>경로4</Box>} />
+      <Route path="/main1/path2" element={<Box>경로4</Box>} />
+      <Route path="/main2">
+        <Route path="path1" element={<Box>경로6</Box>} />
+        <Route path="path2" element={<Box>경로7</Box>} />
+        <Route path="path3">
+          {/* 중첩된 경로까지 완료!*/}
+          <Route path="sub1" element={<Box>경로8</Box>} />
+          <Route path="sub2" element={<Box>경로9</Box>} />
+        </Route>
+      </Route>
     </>,
   ),
 );
