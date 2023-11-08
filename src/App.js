e9d566c4c954fcc522eaadfbe27504acb7ad6ce7
dasom <zzzoo42@naver.com>
2023-11-08 12:28:09 +0900

    useEffect 사용시 depencency 값을 변경 하면 안됨

--- src/App.js
+++ src/App.js
@@ -1,66 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Box, Select, Spinner, Text } from "@chakra-ui/react";
-import axios from "axios";
 
 function App(props) {
-  //직원 번호를 선택 하면 직원의 이름이 출력
-  // /api/main1/sub5?id=5
-  // spring boot의 메소드 작성하기,
-  const [employeesId, setEmployeesId] = useState(0);
-  const [employees, setEmployees] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
+  const [number, setNumber] = useState(0);
 
-  const [employeesIdList, setEmployeesIdList] = useState([]);
-
+  // useEffect 사용시 자주 하는 실수
   useEffect(() => {
-    axios.get("/api/main1/sub7").then((r) => setEmployeesIdList(r.data));
-  }, []);
+    console.log("코드 실행");
+    setNumber(number); // trigger 하는 값을 변경하면 안됨
+  }, [number]);
 
-  useEffect(() => {
-    setIsLoading(true);
-    axios
-      .get("/api/main1/sub5?id=" + employeesId)
-      .then((response) => response.data)
-      .then((data) => setEmployees(data))
-      .catch((error) => setEmployees(null))
-      .finally(() => setIsLoading(false));
-  }, [employeesId]);
-  return (
-    <div>
-      <Select
-        placeholder="직원 번호를 선택하세요🎈"
-        onChange={(e) => setEmployeesId(e.target.value)}
-        background="blue.50"
-      >
-        {/*option[value=$]{$}*/}
-        {employeesIdList.map((id) => (
-          <option value={id}>{id}</option>
-        ))}
-      </Select>
-      <Box>
-        {isLoading && <Spinner />}
-
-        {isLoading || (
-          <>
-            {employees === null ? (
-              <Text>조회한 고객이 없습니다. 다른 번호를 선택해 주세요</Text>
-            ) : (
-              <Text background="#ffd6d1" style={{ color: "white" }}>
-                👉 직원 이름 : {employees.lastName}
-                {employees.firstName}❤️
-                <hr />
-                👉 직원 생일 : {employees.birthDate}🎈
-                <hr />
-                👉 직원 휴대폰 번호 : {employees.photo}🩵
-                <hr />
-                👉 직원 주소 : {employees.notes}💜
-              </Text>
-            )}
-          </>
-        )}
-      </Box>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
